70aa52a1c7fe47036d9315f779ce4e8f44c48dee
Francesco Infante <fra.inf@icloud.com>
2017-05-10 17:34:37 +0200

    use of ' and indexOf

--- src/Valcheck.js
+++ src/Valcheck.js
@@ -63,26 +63,26 @@ const TYPE_ARTICLE = {
 };
 
 const CSS_COLORS = [
-  "aliceblue","antiquewhite","aqua","aquamarine","azure","beige","bisque","black", "blanchedalmond",
-  "blue","blueviolet","brown","burlywood","cadetblue","chartreuse","chocolate","coral",
-  "cornflowerblue","cornsilk","crimson","cyan","darkblue","darkcyan","darkgoldenrod","darkgray",
-  "darkgreen","darkgrey","darkkhaki","darkmagenta","darkolivegreen","darkorange","darkorchid",
-  "darkred","darksalmon","darkseagreen","darkslateblue","darkslategray","darkslategrey",
-  "darkturquoise","darkviolet","deeppink","deepskyblue","dimgray","dimgrey","dodgerblue",
-  "firebrick","floralwhite","forestgreen","fuchsia","gainsboro","ghostwhite","gold","goldenrod",
-  "gray","green","greenyellow","grey","honeydew","hotpink","indianred","indigo","ivory","khaki",
-  "lavender","lavenderblush","lawngreen","lemonchiffon","lightblue","lightcoral","lightcyan",
-  "lightgoldenrodyellow","lightgray","lightgreen","lightgrey","lightpink","lightsalmon",
-  "lightseagreen","lightskyblue","lightslategray","lightslategrey","lightsteelblue","lightyellow",
-  "lime","limegreen","linen","magenta","maroon","mediumaquamarine","mediumblue","mediumorchid",
-  "mediumpurple","mediumseagreen","mediumslateblue","mediumspringgreen","mediumturquoise",
-  "mediumvioletred","midnightblue","mintcream","mistyrose","moccasin","navajowhite","navy",
-  "oldlace","olive","olivedrab","orange","orangered","orchid","palegoldenrod","palegreen",
-  "paleturquoise","palevioletred","papayawhip","peachpuff","peru","pink","plum","powderblue",
-  "purple","red","rosybrown","royalblue","saddlebrown","salmon","sandybrown","seagreen","seashell",
-  "sienna","silver","skyblue","slateblue","slategray","slategrey","snow","springgreen","steelblue",
-  "tan","teal","thistle","tomato","turquoise","violet","wheat","white","whitesmoke","yellow",
-  "yellowgreen"
+  'aliceblue','antiquewhite','aqua','aquamarine','azure','beige','bisque','black', 'blanchedalmond',
+  'blue','blueviolet','brown','burlywood','cadetblue','chartreuse','chocolate','coral',
+  'cornflowerblue','cornsilk','crimson','cyan','darkblue','darkcyan','darkgoldenrod','darkgray',
+  'darkgreen','darkgrey','darkkhaki','darkmagenta','darkolivegreen','darkorange','darkorchid',
+  'darkred','darksalmon','darkseagreen','darkslateblue','darkslategray','darkslategrey',
+  'darkturquoise','darkviolet','deeppink','deepskyblue','dimgray','dimgrey','dodgerblue',
+  'firebrick','floralwhite','forestgreen','fuchsia','gainsboro','ghostwhite','gold','goldenrod',
+  'gray','green','greenyellow','grey','honeydew','hotpink','indianred','indigo','ivory','khaki',
+  'lavender','lavenderblush','lawngreen','lemonchiffon','lightblue','lightcoral','lightcyan',
+  'lightgoldenrodyellow','lightgray','lightgreen','lightgrey','lightpink','lightsalmon',
+  'lightseagreen','lightskyblue','lightslategray','lightslategrey','lightsteelblue','lightyellow',
+  'lime','limegreen','linen','magenta','maroon','mediumaquamarine','mediumblue','mediumorchid',
+  'mediumpurple','mediumseagreen','mediumslateblue','mediumspringgreen','mediumturquoise',
+  'mediumvioletred','midnightblue','mintcream','mistyrose','moccasin','navajowhite','navy',
+  'oldlace','olive','olivedrab','orange','orangered','orchid','palegoldenrod','palegreen',
+  'paleturquoise','palevioletred','papayawhip','peachpuff','peru','pink','plum','powderblue',
+  'purple','red','rosybrown','royalblue','saddlebrown','salmon','sandybrown','seagreen','seashell',
+  'sienna','silver','skyblue','slateblue','slategray','slategrey','snow','springgreen','steelblue',
+  'tan','teal','thistle','tomato','turquoise','violet','wheat','white','whitesmoke','yellow',
+  'yellowgreen'
 ];
 
 /**
@@ -251,7 +251,7 @@ class Valcheck {
     var error;
     if ((error = this.string(key, value, true))) { return error; }
 
-    if (CSS_COLORS.includes(value)) { return; }
+    if (CSS_COLORS.indexOf(value) >= 0) { return; }
 
     if (HEX6_COLOR_RE.test(value)) { return; }
 
