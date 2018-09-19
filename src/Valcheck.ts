49cb881d12b20ac593ea69d6a880d900b8447f8a
davidrapin <david.rapin@gmail.com>
2018-09-19 11:25:25 +0200

    fixing type of FieldDefinition.check

--- src/Valcheck.ts
+++ src/Valcheck.ts
@@ -123,7 +123,7 @@ export interface FieldDefinition<E> {
   deprecated?: string;
   values?: Array<unknown>;
   type?: string | string[];
-  check?: (key: string, value: unknown) => (E | void) | (keyof Valcheck<E>) | string[];
+  check?: (key: string, value: unknown) => (E | void) | [string, ...Array<unknown>] | keyof Valcheck<E>;
   arrayItem?: FieldDefinition<E>;
   arraySize?: number;
   anyProperty?: FieldDefinition<E>;
